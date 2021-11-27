de05e2d31a088db159a626267661c04c9ed0e374
atchernukha <atchernukha@gmail.com>
2021-11-28 01:38:06 +0200

    fix_front_server_1

--- client/server.js
+++ client/server.js
@@ -7,10 +7,13 @@ const PORT = process.env.PORT || 8080
 const app = express()
 app.use(cors())
 
-app.use(express.static(__dirname))
+// app.use(express.static(__dirname))
 app.use(express.static(path.resolve(__dirname, 'build')))
 
-
 app.get('/', (req, res) => res.send('Hello World!'));
+// app.get('/', function (req, res) {
+//     res.sendFile(path.join(__dirname, 'build', 'index.html'));
+//   });
 
-app.listen(PORT)
+// app.listen(PORT)
+app.listen(PORT, () => console.log(`Frontend started on port ${PORT}`))
