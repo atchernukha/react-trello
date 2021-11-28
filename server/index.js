d9f8bbf989cb26ff9cf33f9ce1586345492a7f19
atchernukha <atchernukha@gmail.com>
2021-11-28 06:41:16 +0200

    fix_front_server_9

--- server/index.js
+++ server/index.js
@@ -10,10 +10,11 @@ const PORT = process.env.PORT || 5000
 
 const app = express()
 app.use(cors())
+app.use(express.static(path.resolve(__dirname, '../client/build')));
 app.use(express.json())
 app.use('/api', router)
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
   })
 
 const start = async () => {
