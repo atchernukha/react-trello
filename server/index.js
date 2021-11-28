9b0c37d9c2e5197895bd048c1ecb2aa077a2190f
atchernukha <atchernukha@gmail.com>
2021-11-28 05:56:37 +0200

    fix_front_server_7

--- server/index.js
+++ server/index.js
@@ -11,6 +11,9 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use('/api', router)
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  })
 
 const start = async () => {
     try {
