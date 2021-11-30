6e41a7d4393ed0fd306e7b41c7f976a95af05990
atchernukha <atchernukha@gmail.com>
2021-11-30 05:32:48 +0200

    fix_heroku_deploy_4

--- server/index.js
+++ server/index.js
@@ -2,18 +2,18 @@ require('dotenv').config()
 const express = require('express')
 const sequelize = require('./db')
 const models = require('./models/models')
-const path = require('path')
-const cors = require('cors')
+const path = require('path') // not need for heroku deploy
+const cors = require('cors') // not need for heroku deploy
 const router =require('./routes/index')
 
 const PORT = process.env.PORT || 5000
 
 const app = express()
 app.use(cors())
-app.use(express.static(path.resolve(__dirname, '../client/build'))); // for heroku deploy
+app.use(express.static(path.resolve(__dirname, '../client/build'))); // need only for heroku deploy
 app.use(express.json())
 app.use('/api', router)
-app.get("/*", (req, res) => {res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));  })  // for heroku deploy
+app.get("/*", (req, res) => {res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));  })  // need only for heroku deploy
 
 const start = async () => {
     try {
