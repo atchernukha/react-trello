bf61be530803cb3dffaec86487af801710fb38d3
atchernukha <atchernukha@gmail.com>
2021-12-02 03:01:36 +0200

    link on github

--- README.md
+++ README.md
@@ -10,6 +10,57 @@
 
 You can check the app running clicking [here](https://react-trello-like.herokuapp.com/)
 
+## How to Run
+
+Follow the steps below to run the application on your computed, first of all, you'll need to have these tools installed on your computer: Git, Node.js, NPM or Yarn
+If you have all of these tools installed on your computer, just follow the steps below:
+
+* Clone the repository in some directory of your computer
+>$ git clone git@github.com:atchernukha/react-trello.git
+* Enter in the repository
+>$ cd react-trello
+
+### DATABASE
+* on Postgres server dbserver create new user and new database with owner new user:
+>sudo -u postgres createdb db_name;  
+>sudo -u postgres psql  
+>alter user db_user with password 'db_password';  
+>grant ALL on database db_name to db_user;  
+>q;  
+
+### Backend
+
+* Enter in the server directory
+>$ cd server
+* Create .env file with port and db params:
+
+>PORT=5000  
+>PG_DATABASE=db_name  
+>PG_USER=db_user  
+>PG_PASSWORD=db_password  
+>PG_HOST=dbserver  
+>PG_PORT=5432
+ 
+* Install the dependencies
+>$ npm install
+* Start the app
+>$ npm run dev
+
+### Frontend
+
+* Enter in the client directory
+>$ cd ../client
+* Create .env file with API host params:
+
+>REACT_APP_API_HOST = 'http://localhost:5000/'
+
+* Install the dependencies
+>$ npm install
+* Start the app
+>$ npm start
+
+After following these steps, the terminal will show you in which port it's running, it's on the port 3000 usually, so, you'll just need to enter in your browser and type localhost:3000, then you'll see the app running. When you want to stop it, go to the terminal that you used to start the app, and type CTRL+C, this way you'll stop the app
+
 ## Technical requirements:
 
 * SPA approach for frontend
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
-import { FormControl, Input, InputLabel, InputAdornment, AppBar, Toolbar, Typography, Box, IconButton } from '@mui/material';
+import { FormControl, Input, InputLabel, InputAdornment, AppBar, Toolbar, Typography, Box, IconButton, Link } from '@mui/material';
 import { blue, green, } from '@mui/material/colors';
 import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import GitHubIcon from '@mui/icons-material/GitHub';
 import { useDispatch } from 'react-redux';
 import { ListActionCreators } from '../store/redusers/List/actionCreators';
 
@@ -16,27 +17,30 @@ export default function Header() {
         <Box sx={{ flexGrow: 1 }}>
             <AppBar position="static" sx={{ bgcolor: blue[200] }}>
                 <Toolbar component="form">
-
-                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                    <Typography variant="h4" component="div" sx={{ flexGrow: 1 }}>
                         React Trello-like App
+                        <Link href="https://github.com/atchernukha/react-trello" underline="hover" variant="h6" sx={{ mx: "20px", }} >
+                        {'<source: /> '}
+                        <GitHubIcon/>
+                    </Link>
                     </Typography>
 
                     <FormControl sx={{ mx: "20px", }} variant="standard">
-                    <InputLabel htmlFor="new-item">Type list title...</InputLabel>
-                    <Input
-                        id="new-item"
-                        variant="standard"
-                        value={listName}
-                        onChange={e => { setlistName(e.target.value) }}
-                        endAdornment={
-                            <InputAdornment position="end">
-                                <IconButton  type="submit" disabled={!listName} onClick={addList} size="small" sx={{ color: green[400] }}>
-                                    <AddCircleOutlineIcon />
-                                </IconButton>
-                            </InputAdornment>
-                        }
-                    />
-                </FormControl>
+                        <InputLabel htmlFor="new-item">Type list title...</InputLabel>
+                        <Input
+                            id="new-item"
+                            variant="standard"
+                            value={listName}
+                            onChange={e => { setlistName(e.target.value) }}
+                            endAdornment={
+                                <InputAdornment position="end">
+                                    <IconButton type="submit" disabled={!listName} onClick={addList} size="small" sx={{ color: green[400] }}>
+                                        <AddCircleOutlineIcon />
+                                    </IconButton>
+                                </InputAdornment>
+                            }
+                        />
+                    </FormControl>
                 </Toolbar>
             </AppBar>
         </Box>
